bb130b486c34b635e0d4084897752d67313aa7f5
Ahmad Ali Zarei <64613847+ahmadalizarei@users.noreply.github.com>
2022-10-05 12:12:47 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const app = express();
+var http = require('http');
 
-app.get('/', (req, res) => {
-  res.send('Hello from App Engine!');
+var app = http.createServer(function(req,res){
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ a: 1 }));
 });
 
-// Listen to the App Engine-specified port, or 8080 otherwise
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
