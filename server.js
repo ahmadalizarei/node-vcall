c8d69a7bf1a1ac2ff24de59ddcc3a6086f96d584
Ahmad Ali Zarei <64613847+ahmadalizarei@users.noreply.github.com>
2022-10-06 02:57:41 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 
-var cors = require('cors');
-
 
 
 var http = require('http');
 
 var app = http.createServer(function(req,res){
+    res.set('Access-Control-Allow-Origin', '*');
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify({ "a": "1" }));
 });
 
-app.use(cors());
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
