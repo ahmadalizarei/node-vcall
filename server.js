25966a218bc448ba15140008307e898835584ec1
Ahmad Ali Zarei <64613847+ahmadalizarei@users.noreply.github.com>
2022-10-05 12:31:20 +0330

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var app = http.createServer(function(req,res){
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ a: 1 }));
+    res.end(JSON.stringify({ "a": "1" }));
 });
 
 
