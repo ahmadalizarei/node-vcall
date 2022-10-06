1fbcf1c78edc75b64ab8a05b0f8b371cd2904b70
Ahmad Ali Zarei <64613847+ahmadalizarei@users.noreply.github.com>
2022-10-06 11:51:56 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,84 @@
 
+const express = require('express');
+var router = express.Router();
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+
+    cors: {
+
+        origin:  "https://peppy-dasik-dc6443.netlify.app/",
+        methods: [ "GET" , "POST" ]
+    }
+})
+const path = require('path');
 
 
-var http = require('http');
 
-var app = http.createServer(function(req,res){
-    res.set('Access-Control-Allow-Origin', '*');
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ "a": "1" }));
+
+
+
+io.on('connection', (socket) => {
+  sid=socket.id;
+  io.to(socket.id).emit("sid",socket.id);
+
+  console.log(socket.id+" :user connected");
+  socket.on('disconnect', () => {
+    console.log(socket.id+'user disconnected');
+  }
+
+);
+
+  
+
+  
+
+
+ socket.on('chat message', (msg,p) => {
+  
+  io.emit('chat message',p+":" + msg);
+  
+ });
+
+
+
+
+
+ 
+
+ 
+ 
 });
 
+var device = require('express-device');
+app.use(device.capture());
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`);
-});
+
+
+
+app.get('/st2.css', function (req, res) {
+  res.sendFile(path.join(__dirname, '/public/st2.css'));
+})
+
+
+app.get('/st.css', function (req, res) {
+  res.sendFile(path.join(__dirname, '/public/st.css'));
+})
+app.get('/particle.js', function (req, res) {
+  res.sendFile(path.join(__dirname, '/public/particle.js'));
+})
+
+
+app.get('/index', function (req, res) {
+  if(req.device.type.toUpperCase()=="PHONE")
+
+  res.sendFile(path.join(__dirname, '/index.html'));
+  else 
+  res.sendFile(path.join(__dirname, '/index2.html'));
+})
+
+
+
+server.listen(process.env.PORT || 3000, () => { console.log("connect") });
+//server.listen(5000, () => { console.log("connect") })
